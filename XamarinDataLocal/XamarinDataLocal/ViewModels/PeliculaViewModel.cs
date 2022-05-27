fc06cd93733e577eb2e49c5d49894890daf4b2e7
serraguti <pacoserranox@gmail.com>
2022-05-27 12:34:16 +0200

    commit

---
+++ XamarinDataLocal/XamarinDataLocal/ViewModels/PeliculaViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using XamarinDataLocal.Base;
+using XamarinDataLocal.Models;
+
+namespace XamarinDataLocal.ViewModels
+{
+    public class PeliculaViewModel: ViewModelBase
+    {
+        private Pelicula _Pelicula;
+        public Pelicula Pelicula
+        {
+            get { return this._Pelicula; }
+            set {
+                this._Pelicula = value;
+                OnPropertyChanged("Pelicula");
+            }
+        }
+    }
+}
